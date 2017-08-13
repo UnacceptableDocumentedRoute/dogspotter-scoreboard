e658733dfc4edea1aced8da89c20d34ad1eb96a9
Alphys-Hedge <Alpha-Hedge@users.noreply.github.com>
2017-08-12 21:26:02 -0500

    Update dbhandling.js

--- dbhandling.js
+++ dbhandling.js
@@ -9,3 +9,27 @@ firebase.initializeApp(config);
 
 // Get a reference to the database service
 var database = firebase.database();
+
+// firebase.database().ref('users/')
+
+// Authentication
+var provider = new firebase.auth.GoogleAuthProvider();
+firebase.auth().signInWithRedirect(provider);
+
+// Create a function to get the value of data
+// function getValue()
+// {
+// 	return firebase.database().ref('/test/p').once('value').then(function(snapshot) {
+// 	var username = snapshot.val().username;
+// 	});
+// }
+
+// Create add points function (NOT FINSIHED!)
+// function addPoints(name,value) 
+// {
+// 	firebase.database().ref(name).set({
+// 		name: value,
+// 	});
+// }
+
+// addPoints(test,20);
