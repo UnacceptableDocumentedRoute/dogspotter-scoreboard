865e88beff31c0a81e90a3034b88c50a818d4ccd
Alphys-Hedge <Alpha-Hedge@users.noreply.github.com>
2017-08-12 22:06:40 -0500

    Update dbhandling.js

--- dbhandling.js
+++ dbhandling.js
@@ -10,24 +10,35 @@ firebase.initializeApp(config);
 // Get a reference to the database service
 var database = firebase.database();
 
-// firebase.database().ref('users/')
+// Using a redirect.
+firebase.auth().getRedirectResult().then(function(result) 
+{
+	if (result.credential) 
+	{
+		// This gives you a Google Access Token.
+		var token = result.credential.accessToken;
+	}
+	var user = result.user;
+});
 
-// Authentication
+// Start a sign in process for an unauthenticated user.
 var provider = new firebase.auth.GoogleAuthProvider();
+provider.addScope('profile');
+provider.addScope('email');
 firebase.auth().signInWithRedirect(provider);
 
 // Create a function to get the value of data
-// function getValue()
-// {
-// 	return firebase.database().ref('/test/p').once('value').then(function(snapshot) {
-// 	var username = snapshot.val().username;
-// 	});
-// }
+function getValue()
+{
+	return firebase.database().ref('/test/p').once('value').then(function(snapshot) {
+	var username = snapshot.val().username;
+	});
+}
 
-// Create add points function (NOT FINSIHED!)
-// function addPoints(name,value) 
-// {
-// 	firebase.database().ref(name).set({
-// 		name: value,
-// 	});
-// }
+// Create add points function
+function addPoints(name,value) 
+{
+	firebase.database().ref(name).set({
+		name: value,
+	});
+}
