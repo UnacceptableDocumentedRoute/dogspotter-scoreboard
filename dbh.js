463fa7e0c6e4e4a3a57c7997eedd843af3bfcbe5
Alpha Hedge <ultimategfbcfan@gmail.com>
2017-08-15 13:10:55 -0500

    Update dbh.js

--- dbh.js
+++ dbh.js
@@ -10,34 +10,10 @@ firebase.initializeApp(config);
 // Get a reference to the database service
 var database = firebase.database();
 
-// Using a redirect.
-firebase.auth().getRedirectResult().then(function(result) 
-{
-	if (result.credential) 
-	{
-		// This gives you a Google Access Token.
-		var token = result.credential.accessToken;
-	}
-	var user = result.user;
-});
-
-// Start a sign in process for an unauthenticated user.
-var provider = new firebase.auth.GoogleAuthProvider();
-provider.addScope('profile');
-provider.addScope('email');
-
 // Create a function to get the value of data
-function getValue()
+function getValue(name)
 {
-	return firebase.database().ref('/test/p').once('value').then(function(snapshot) {
+	return firebase.database().ref(name).once('value').then(function(snapshot) {
 	var username = snapshot.val().username;
-	});
-}
-
-// Create add points function
-function addPoints(name,value) 
-{
-	firebase.database().ref(name).set({
-		name: value,
 	});
 }
